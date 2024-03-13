57afcd11f1a740e7b2ea2113346b89e698bdaf91
Burinchai <644230046@webmail.npru.ac.th>
2024-03-13 14:27:54 +0700

    NewLastest

--- client/src/admin/components/ConnectWeb3.jsx
+++ client/src/admin/components/ConnectWeb3.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
+import EventEmitter from 'events';
 
 function ButtonMetamask() {
   const [account, setAccount] = useState('');
